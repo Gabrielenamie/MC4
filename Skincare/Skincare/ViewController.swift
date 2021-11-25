f4c88914412353a52c71f34db486150bccdfdb51
mviniss <m_vinicius.ss@icloud.com>
2021-11-25 17:31:58 -0300

    Criação do projeto
    Criando o projeto

---
+++ Skincare/Skincare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skincare
+//
+//  Created by Marcus Vinicius Silva de Sousa on 25/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
