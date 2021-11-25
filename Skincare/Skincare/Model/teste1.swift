c1b9b2b5950edf315aa3d3c7b525433c28c368a4
Gabriele Namie <gabrielenamie@hotmail.com>
2021-11-25 18:01:30 -0300

    criacao .swift

---
+++ Skincare/Skincare/Model/teste1.swift
@@ -0,0 +1,8 @@
+//
+//  teste1.swift
+//  Skincare
+//
+//  Created by Gabriele Namie on 25/11/21.
+//
+
+import Foundation
---
+++ Skincare/Skincare/View/teste.swift
@@ -0,0 +1,8 @@
+//
+//  teste.swift
+//  Skincare
+//
+//  Created by Gabriele Namie on 25/11/21.
+//
+
+import Foundation
