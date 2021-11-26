306e4718ea859173dd3eedce9f7dcb6e1e5644dc
Gabriele Namie <gabrielenamie@hotmail.com>
2021-11-26 16:24:58 -0300

    apagar

--- Skincare/Skincare/Model/teste1.swift
+++
@@ -1,8 +0,0 @@
-//
-//  teste1.swift
-//  Skincare
-//
-//  Created by Gabriele Namie on 25/11/21.
-//
-
-import Foundation
--- Skincare/Skincare/View/teste.swift
+++
@@ -1,8 +0,0 @@
-//
-//  teste.swift
-//  Skincare
-//
-//  Created by Gabriele Namie on 25/11/21.
-//
-
-import Foundation
