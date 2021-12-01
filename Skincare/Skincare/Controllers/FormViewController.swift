e72fd55cae228e1bbb5d43def91cb24fe8d40083
Gabriele Namie <gabrielenamie@hotmail.com>
2021-12-01 19:22:13 -0300

    arrumar forms

--- Skincare/Skincare/Controllers/FormViewController.swift
+++ Skincare/Skincare/Controllers/FormViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class FormViewController: UIViewController {
     let questions = ["Quantas vezes por dia você sente necessidade de lavar o rosto?","Como você descreveria sua pele?", "Poros são buraquinhos na pele. Ao se olhar no espelho, você consegue observá-los?","Você costuma ter espinhas ou cravos?","Depois de praticar uma atividade física, seu rosto fica como?"]
-    let answers = [["Em dias úmidos ou no verão sinto mais necessidade", "Só uma vez, geralmente para tirar a maquiagem", "Várias vezes,sinto minha pele com aspecto sujo", "Não sinto necessidade, somente no banho",],
-                   ["Em alguns locais parece oleosa e em outros não", "Não possuo oleosidade aparente, estando sempre com aspecto hidratado", "Muita oleosidade por todo o rosto, podendo aparecer espinhas","Minha pele é opaca e sem brilho, às vezes avermelhada ou recssecada",],
-                   ["Posso vê-los mais no queixo, nariz ou testa", "Não muito, são pouco aparentes","Sim, posso ver no rosto todo","Não, minha pele é completamente lisa",],
-                   ["De vez em quando","É bem difícil","Quase sempre","Nunca",],
-                   ["Um pouco oleoso e suado", "Com as bochechas avermelhadas e suado", "Muito oleoso e 'pegajoso'","Avermelhado e ardendo",]]
+    let answers = [["Em dias úmidos ou no verão sinto mais necessidade", "Só uma vez, geralmente para tirar a maquiagem", "Várias vezes,sinto minha pele com aspecto sujo", "Não sinto necessidade, somente no banho"],
+                   ["Em alguns locais parece oleosa e em outros não", "Não possuo oleosidade aparente, estando sempre com aspecto hidratado", "Muita oleosidade por todo o rosto, podendo aparecer espinhas","Minha pele é opaca e sem brilho, às vezes avermelhada ou recssecada"],
+                   ["Posso vê-los mais no queixo, nariz ou testa", "Não muito, são pouco aparentes","Sim, posso ver no rosto todo","Não, minha pele é completamente lisa"],
+                   ["De vez em quando","É bem difícil","Quase sempre","Nunca"],
+                   ["Um pouco oleoso e suado", "Com as bochechas avermelhadas e suado", "Muito oleoso e 'pegajoso'","Avermelhado e ardendo"]]
     
     var currentQuestion = 0
     var oilyAnswers = 0
@@ -28,9 +28,12 @@ class FormViewController: UIViewController {
 
     @IBAction func nextButton(_ sender: Any) {
         if currentQuestion != questions.count{
+            currentQuestion += 1
+            if currentQuestion > 4{
+              performSegue(withIdentifier: "choiceView", sender: self)
+            }else{
             newQuestion()
-        }else{
-            performSegue(withIdentifier: "choiceView", sender: self)
+            }
         }
     }
 
@@ -42,10 +45,8 @@ class FormViewController: UIViewController {
         for i in 1...4{
             button = view.viewWithTag(i)as! UIButton
             button.setTitle(answers[currentQuestion][x], for: .normal)
-            
             x += 1
         }
-            currentQuestion += 1
     }
     override func viewWillAppear(_ animated: Bool) {
         newQuestion()
