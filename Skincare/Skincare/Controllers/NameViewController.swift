bb37a908d37cfb2e68fca13e08ae6f878e3cb6da
Carolina Ortega <cahortega@outlook.com>
2021-11-30 21:50:47 -0300

    Criação das ViewControllers

---
+++ Skincare/Skincare/Controllers/NameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NameViewController.swift
+//  Skincare
+//
+//  Created by Carolina Ortega on 30/11/21.
+//
+
+import UIKit
+
+class NameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- Skincare/Skincare/Controllers/OnboardingViewController.swift
+++ Skincare/Skincare/Controllers/OnboardingViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OnboardingViewController: UIViewController {
 //    @IBOutlet weak var progressVIew: UIProgressView!
 //    let progress = Progress(totalUnitCount: 9)
     
