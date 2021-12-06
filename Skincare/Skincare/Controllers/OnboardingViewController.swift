de9a58f28a9de09a7f038acc6b7364a60a8a1375
Gabriel Batista <gabriel.devproject@gmail.com>
2021-12-06 13:53:41 -0300

    Revert "Feat front end"

--- Skincare/Skincare/Controllers/OnboardingViewController.swift
+++ Skincare/Skincare/Controllers/OnboardingViewController.swift
@@ -8,22 +8,21 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
-    //    @IBOutlet weak var progressVIew: UIProgressView!
-    //    let progress = Progress(totalUnitCount: 9)
+//    @IBOutlet weak var progressVIew: UIProgressView!
+//    let progress = Progress(totalUnitCount: 9)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.setHidesBackButton(true, animated: false)
-        
+        // Do any additional setup after loading the view.
     }
     
-    //    @IBAction func botao(_ sender: Any) {
-    //
-    //            self.progress.completedUnitCount += 1
-    //
-    //            let progressfloat = Float(self.progress.fractionCompleted)
-    //            self.progressVIew.setProgress(progressfloat, animated: true)
-    //        }
-    
+//    @IBAction func botao(_ sender: Any) {
+//       
+//            self.progress.completedUnitCount += 1
+//            
+//            let progressfloat = Float(self.progress.fractionCompleted)
+//            self.progressVIew.setProgress(progressfloat, animated: true)
+//        }
+
 }
 
